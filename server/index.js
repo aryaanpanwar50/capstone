12c69681512de4cd249047e703f84e422588d0b7
Aryaan007-sudo <blankgaming75@gmail.com>
2025-04-14 14:56:58 +0530

    changss

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['http://localhost:5174','https://capstone-puce-rho.vercel.app/'],
+  origin: ['http://localhost:5174','https://capstone-puce-rho.vercel.app'],
   credentials: true
 }));
 
