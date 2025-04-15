cc602b0fb2c0f3c06e3bbe5c16526e7acf24eddb
Aryaan007-sudo <blankgaming75@gmail.com>
2025-04-15 15:35:28 +0530

    404 error

---
+++ client/src/Pages/FingerAuth.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NotFound404 from './NotFound404'
+
+function FingerAuth() {
+  return (
+    <div><NotFound404></NotFound404></div>
+  )
+}
+
+export default FingerAuth
