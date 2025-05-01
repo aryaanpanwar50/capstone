7843aa8d61529706a32278d9439a84a29d38c6f5
Aryaan007-sudo <blankgaming75@gmail.com>
2025-05-01 11:40:42 +0530

    changes

--- server/config/passport.js
+++ server/config/passport.js
@@ -19,7 +19,10 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8080/auth/google/callback",
+    callbackURL: process.env.NODE_ENV === 'production'
+    ? 'https://your-render-service.onrender.com/auth/google/callback'
+    : 'http://localhost:8080/auth/google/callback'
+,
     proxy: true
 },
 async (accessToken, refreshToken, profile, done) => {
